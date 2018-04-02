75f3c5a7a6573d7eab61d4329c81682682ddafcd
bteo <bteo@andrew.cmu.edu>
2018-04-02 13:14:03 -0400

    Minor corrections to demos 3D11/12/20

--- demos/3D/demo3D12/demo3D12.sh
+++ demos/3D/demo3D12/demo3D12.sh
@@ -42,7 +42,7 @@
 # For additional information visit http://murphylab.web.cmu.edu or
 # send email to murphy@cmu.edu
 
-directory=../../../images/HeLa/2D/LAM
+directory=../../../images/HeLa/3D/processed
 
 if [ ! -d "$directory" ]; then
 	wget -nc --quiet http://www.cellorganizer.org/Downloads/v2.7/docker/v2.7.1/images/demo3D12.tgz
@@ -83,7 +83,7 @@ options.model.filename = 'LAMP2.xml';" > input.txt
 
 img2slml $(pwd)/input.txt
 
-if [ ! -f $(pwd)/lamp2.mat ];
-	echo "File "$(pwd)"/lamp2.mat does not exist"
+if [ ! -f $(pwd)/LAMP2.mat ]; then
+	echo "File "$(pwd)"/LAMP2.mat does not exist"
 	exit -1
 fi
